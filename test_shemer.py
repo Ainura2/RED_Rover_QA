e4d1742557c1ecdcde4951f40c62b8776d74e05c
Ainura2 <agmusagulova1990@gmail.com>
2023-11-23 12:31:34 +0800

    test_case1

---
+++ test_shemer.py
@@ -0,0 +1,38 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import pytest
+
+
+options = Options()
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+
+def test_women():
+    driver.get('https://magento.softwaretestingboard.com/')
+    sale_button = driver.find_element(By.XPATH, "//*[@id='store.menu']//*[text()='Sale']/parent::a")
+    sale_button.click()
+    assert driver.title == "Sale"
+
+    women_button = driver.find_element(By.XPATH, "//*[@id='maincontent']//*[text()='Shop Women’s Deals']")
+    women_button.click()
+    assert driver.title == "Women Sale"
+    driver.get_screenshot_as_file("tr.png")
+    driver.close()
+
+
+def test_right():
+    driver.get('https://magento.softwaretestingboard.com/')
+    sale_button = driver.find_element(By.XPATH, "//*[@id='store.menu']//*[text()='Sale']/parent::a")
+    sale_button.click()
+    assert driver.title == "Sale"
+
+    women_button = driver.find_element(By.XPATH, "//*[@id='maincontent']//*[text()='Shop Women’s Deals']")
+    women_button.click()
+    assert driver.title == "Women Sale"
+    driver.get_screenshot_as_file("tr.png")
+    driver.close()
