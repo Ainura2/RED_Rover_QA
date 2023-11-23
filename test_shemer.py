666958b29fefbfac4cc0c3d08f9793b2415e699c
Ainura2 <agmusagulova1990@gmail.com>
2023-11-23 17:49:55 +0800

    test_case1

--- test_shemer.py
+++ test_shemer.py
@@ -4,6 +4,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import Actions
 import pytest
 
 
@@ -28,11 +31,14 @@ def test_women():
 def test_right():
     driver.get('https://magento.softwaretestingboard.com/')
     sale_button = driver.find_element(By.XPATH, "//*[@id='store.menu']//*[text()='Sale']/parent::a")
-    sale_button.click()
+    action = ActionChains(driver)
+    action.key_down(Keys.CONTROL)
+    action.click(on_element=sale_button)
+
+    action.perform()
+
+    driver.switch_to.window(driver.window_handles[1])
     assert driver.title == "Sale"
 
-    women_button = driver.find_element(By.XPATH, "//*[@id='maincontent']//*[text()='Shop Women’s Deals']")
-    women_button.click()
-    assert driver.title == "Women Sale"
-    driver.get_screenshot_as_file("tr.png")
+    # driver.save_screenshot("tr2.png")
     driver.close()
